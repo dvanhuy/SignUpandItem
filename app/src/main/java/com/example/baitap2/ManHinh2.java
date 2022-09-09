ed4eba20244d437b02998938f83b5bdbc75acddf
unknown <huydinhvan13@gmail.com>
2022-09-09 18:00:50 +0700

    first commit

---
+++ app/src/main/java/com/example/baitap2/ManHinh2.java
@@ -0,0 +1,16 @@
+package com.example.baitap2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class ManHinh2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh2);
+        getSupportActionBar().hide();
+    }
+}
